49234c347ebbd6d907c47306dd04ab525e423a7d
Albert <ayanit1@googlemail.com>
2017-04-20 23:37:37 +0100

    Add spec for player moves

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,6 +2,7 @@ require 'game'
 
 describe Game do
   subject(:game) { described_class.new }
+  let(:first_move) { 1 }
 
   context 'when instantiated' do
     it 'can hold moves' do
@@ -10,6 +11,16 @@ describe Game do
 
     it 'no moves have been made' do
       expect(game.moves).to be_empty
+    end
+
+    xit 'has winning combinations' do
+      expect(game.winning_combo).to be_a(Win_combos)
+    end
+  end
+
+  context 'player makes a move' do
+    it 'adds to .move' do
+      expect{ game.player_move(first_move) }.to change{ game.moves.size }.by(1)
     end
   end
 end
