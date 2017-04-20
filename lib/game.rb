17222f3d301170b6d2dc260f096de768a3d399e0
Albert <ayanit1@googlemail.com>
2017-04-20 23:40:57 +0100

    Add player move feature

--- lib/game.rb
+++ lib/game.rb
@@ -1,8 +1,23 @@
 class Game
-
   attr_reader :moves
 
   def initialize
     @moves =[]
+
+    #Winning combinations
+    w1 = Set.new [1,2,3]
+    w2 = Set.new [4,5,6]
+    w3 = Set.new [7,8,9]
+    w4 = Set.new [1,4,7]
+    w5 = Set.new [2,5,8]
+    w6 = Set.new [3,6,9]
+    w7 = Set.new [1,5,9]
+    w8 = Set.new [3,5,7]
+
+    @winning_combos = [w1,w2,w3,w4,w5,w6,w7,w8]
+  end
+
+  def player_move(num)
+    @moves.push(num)
   end
 end
