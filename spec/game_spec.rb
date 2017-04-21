ec1ec70101c6c744f251b249eb838c51b2550d64
Albert <ayanit1@googlemail.com>
2017-04-21 12:44:34 +0100

    Add specs for winning game

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -25,13 +25,36 @@ describe Game do
     end
 
     context 'game can be drawn' do
-      it 'raises draw message if more than 5 moves exceeded' do
+      it 'raises draw message' do
         draw_game = Game.new
         draw_game.player_move(1)
         draw_game.player_move(3)
         draw_game.player_move(5)
         draw_game.player_move(8)
         expect{ draw_game.player_move(6) }.to raise_error("Draw")
+      end
+    end
+
+    context 'game can be won' do
+      it 'in a row' do
+        win_game = Game.new
+        win_game.player_move(1)
+        win_game.player_move(2)
+        expect { win_game.player_move(3) }.to raise_error("Win")
+      end
+
+      it 'in a column' do
+        win_game = Game.new
+        win_game.player_move(1)
+        win_game.player_move(4)
+        expect { win_game.player_move(7) }.to raise_error("Win")
+      end
+
+      it 'in a diagonal' do
+        win_game = Game.new
+        win_game.player_move(1)
+        win_game.player_move(5)
+        expect { win_game.player_move(9) }.to raise_error("Win")
       end
     end
   end
